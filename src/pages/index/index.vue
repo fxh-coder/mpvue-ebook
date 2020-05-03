dc18df47660b5dd643942b546a595fdd7be951e9
fxh <3314965946@qq.com>
2020-05-03 22:06:35 +0800

    mpvue多端应用小程序初始化，以及简单组件开发完成

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <SearchBar
+      disabled
+      @onClick="onSearchBarClick"
+    />
+    <HomeCard />
+    <HomeBanner
+      img="http://www.youbaobao.xyz/book/res/bg.jpg"
+      title="mpvue2.0实战多端小程序上线啦"
+      subTitle="立即体验"
+      @click="onBannerClick"
+    />
+  </div>
+</template>
+
+<script>
+import SearchBar from '../../components/home/SearchBar'
+import HomeCard from '../../components/home/HomeCard'
+import HomeBanner from '../../components/home/HomeBanner'
+
+export default {
+  components: {
+    SearchBar,
+    HomeCard,
+    HomeBanner
+  },
+  methods: {
+    onSearchBarClick() {},
+    onBannerClick() {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
